ee6b1b840a9a0a81ebe45fdcb58e50766a8b2387
Tim Evans <tim_evans@condenast.com>
2017-03-15 16:21:28 -0400

    fetch countries instead of having a tiny list

--- tests/dummy/app/index/route.js
+++ tests/dummy/app/index/route.js
@@ -2,13 +2,6 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   model() {
-    return {
-      role: 'owner',
-      roles: [
-        'collaborator',
-        'maintainer',
-        'owner'
-      ]
-    };
+    return Ember.$.get('https://restcountries.eu/rest/v2/all');
   }
 });
