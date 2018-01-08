1fc5d399cb9988256310a72dcbbe09f3c7629649
Tim Evans <tim.c.evans@me.com>
2018-01-07 23:36:09 -0500

    remove vendor styles

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 /* global node: true */
 module.exports = {
-  name: 'ember-single-select',
-
-  included: function (app) {
-    this._super.included(app);
-    app.import("vendor/styles/ember-single-select.css");
-  }
+  name: 'ember-single-select'
 };
